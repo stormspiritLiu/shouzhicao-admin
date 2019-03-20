6265a28145913c8a677001e02ef379ceb9000a51
lzl <a912883181@163.com>
2019-03-20 16:13:37 +0800

    基础模板上传

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -12,6 +12,19 @@ class Index extends Base
 
     public function index()
     {
+        $version = Db::query('SELECT VERSION() AS ver');
+        $config  = [
+            'url'             => $_SERVER['HTTP_HOST'],
+            'document_root'   => $_SERVER['DOCUMENT_ROOT'],
+            'server_os'       => PHP_OS,
+            'server_port'     => $_SERVER['SERVER_PORT'],
+            'server_ip'       => $_SERVER['SERVER_ADDR'],
+            'server_soft'     => $_SERVER['SERVER_SOFTWARE'],
+            'php_version'     => PHP_VERSION,
+            'mysql_version'   => $version[0]['ver'],
+            'max_upload_size' => ini_get('upload_max_filesize')
+        ];
+        $this->assign('config', $config);
         return $this->fetch();
     }
 }
