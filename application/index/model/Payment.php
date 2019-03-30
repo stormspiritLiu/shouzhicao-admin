85ee387237577b978e16cd52e4768584ddb82942
lzl <a912883181@163.com>
2019-03-30 11:41:04 +0800

    payment.type修改返回字段

--- application/index/model/Payment.php
+++ application/index/model/Payment.php
@@ -6,7 +6,7 @@ use think\Model;
 class Payment extends Model
 {
     public function pagingQuery($list_rows,$page,$key){
-        return Payment::order('p.id')
+        $result = Payment::order('p.id')
             ->alias('p')
             ->leftJoin('user u','p.userId = u.id')
             ->leftJoin('game g','p.gameId = g.id')
@@ -14,5 +14,13 @@ class Payment extends Model
             ->field('p.id,u.name as userName,g.name as gameName, g.price,p.type, p.time')
             ->paginate(array('list_rows' => $list_rows, 'page' => $page))
             ->toArray();
+        foreach ($result['data'] as $key => $value){
+            if($value['type'] == 1){
+                $result['data'][$key]['type'] = '健康豆';
+            }else {
+                $result['data'][$key]['type'] = '钻石';
+            }
+        }
+        return $result;
     }
 }
